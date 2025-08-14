b736fcd5eb3c936a93c0b824bdd0c0fa919cc493
eshnag04 <eshna.mkc@gmail.com>
2025-08-14 12:40:54 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // If you deploy to GitHub Pages under https://<you>.github.io/passion-dsa/
-  base: '/passion-dsa/',
+  base: '/passionDSA/',
   server: {
     proxy: { '/api': { target: 'http://localhost:8787', changeOrigin: true } }
   }
